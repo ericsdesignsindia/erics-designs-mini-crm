57007977fd3242ae9a31b7756ede983dbda1c0cc
RenEric <reneric456@gmail.com>
2026-09-17 12:32:55 +0530

    Add recurring invoice schedules

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='erics-crm-v20260917-9';
-const ASSETS=['./','./index.html','./style.css?v=20260917-9','./crm.css?v=20260917-9','./billing.js?v=20260917-9','./crm.js?v=20260917-9','./api-sync.js?v=20260917-9','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png'];
+const CACHE='erics-crm-v20260917-10';
+const ASSETS=['./','./index.html','./style.css?v=20260917-10','./crm.css?v=20260917-10','./billing.js?v=20260917-10','./crm.js?v=20260917-10','./api-sync.js?v=20260917-10','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request,{ignoreSearch:false}).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>cached)))});
