e3026dd90d11ccc13ffcc45f9ee815a4af05ebf3
RenEric <reneric456@gmail.com>
2026-09-19 16:31:26 +0530

    Force dashboard headline contrast

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='erics-crm-v20260919-58';
-const ASSETS=['./','./index.html','./style.css?v=20260919-58','./crm.css?v=20260919-58','./billing.js?v=20260919-58','./crm.js?v=20260919-58','./api-sync.js?v=20260919-58','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png'];
+const CACHE='erics-crm-v20260919-59';
+const ASSETS=['./','./index.html','./style.css?v=20260919-59','./crm.css?v=20260919-59','./billing.js?v=20260919-59','./crm.js?v=20260919-59','./api-sync.js?v=20260919-59','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
