1bc567f83c2fe3b19e72318972953178ea2bc97b
RenEric <reneric456@gmail.com>
2026-09-26 15:04:59 +0530

    Sync PDF delivery files to published bundle

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='erics-crm-v20260926-01';
-const ASSETS=['./','./index.html','./style.css?v=20260920-67','./crm.css?v=20260926-01','./billing.js?v=20260920-67','./crm.js?v=20260926-01','./api-sync.js?v=20260920-67','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png','./assets/erics-designs-crm-logo.png','./assets/payment-received-seal.svg','./assets/quotation-accepted-seal.svg'];
+const CACHE='erics-crm-v20260926-18';
+const ASSETS=['./','./index.html','./style.css?v=20260920-67','./crm.css?v=20260926-18','./billing.js?v=20260920-67','./crm.js?v=20260926-18','./api-sync.js?v=20260926-18','./site.webmanifest','./ed-icon-192.png','./ed-icon-512.png','./assets/erics-designs-crm-logo.png','./assets/payment-received-seal.svg','./assets/quotation-accepted-seal.svg','./assets/jspdf.umd.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
